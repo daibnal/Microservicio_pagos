706dd612e12ddc384951d11668a740496c92caa0
dairys <dai.bernal@duocuc.cl>
2026-05-23 23:26:39 -0400

    se agrego al pago la columna método, transacción y factura

--- pagos/pagos/src/main/java/duoc/dairys/pagos/model/Pagos.java
+++ pagos/pagos/src/main/java/duoc/dairys/pagos/model/Pagos.java
@@ -7,8 +7,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -27,9 +30,28 @@ public class Pagos {
 
     @OneToOne(mappedBy = "pagos", cascade = CascadeType.ALL, orphanRemoval = true)
     private Long idCliente;
+
+    @NotNull(message = "El monto es obligatorio")
     private Double monto;
+
+     @NotNull(message = "La fecha es obligatoria")
     private LocalDateTime fechaPago;
+
+    @NotBlank(message = "El estado no puede estar vacio")
     private String estado;
+
+    @OneToOne
+    @JoinColumn(name = "meotod_pago_id")
+    private MetodoPago metodoPago;
+
+    @OneToOne
+    @JoinColumn(name = "transaccion_id")
+    private Transaccion transaccion;
+
+    @OneToOne
+    @JoinColumn(name = "factura_id")
+    private Factura factura;
+
 
     public Pagos(Long idPago, Long idPedido, Long idCliente, Double monto, LocalDateTime fechaPago, String estado){
         this.idPago = idPago;
